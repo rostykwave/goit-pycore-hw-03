5a9dd9b71c06a31b78b39bd5262d98d1628787b6
Rostyslav <rostykwave@gmail.com>
2024-07-14 17:45:52 +0300

    feat/task-1: add exception

--- task-1.py
+++ task-1.py
@@ -1,10 +1,14 @@
 from datetime import datetime
 
 def get_days_from_today(date):
-    given_date = datetime.strptime(date, "%Y-%m-%d").date()
-    todays_date = datetime.now().date()
-    delta_in_days = (todays_date - given_date).days
-    return delta_in_days
-
+    try:
+        given_date = datetime.strptime(date, "%Y-%m-%d").date()
+        todays_date = datetime.now().date()
+        delta_in_days = (todays_date - given_date).days
+        return delta_in_days
+    except ValueError:
+        return "Incorrect date format. Use the 'YYYY-MM-DD' format instead."
 
 print(get_days_from_today("2020-10-09"))
+print(get_days_from_today("2025-10-09"))
+print(get_days_from_today("Incorrect date format"))
